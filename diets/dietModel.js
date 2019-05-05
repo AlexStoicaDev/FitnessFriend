0af12152a851de7d59e6e89eba34d5067b9c5e59
Spetrut <petrutstoica123@yahoo.com>
2019-05-05 19:26:30 +0300

    Create Mongoose model for Food and Diet

---
+++ diets/dietModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+require("/FitnessFriend/users/userModel.js");
+require("/FitnessFriend/foods/foodModel.js");
+
+const dietSchema = new mongoose.Schema({
+  currentDiet: {
+    type: Boolean,
+    required: true
+  },
+  goal: {
+    type: String,
+    enum: ["Loose", "Mantain", "Gain"]
+  },
+  startingWieght: {
+    type: Number,
+    required: true
+  },
+  goalWeight: {
+    type: Number,
+    required: true
+  },
+  foodType: {
+    type: String,
+    enum: ["vegan", "vegetarian", "omnivor"]
+  },
+
+  weighIns: [{ date: Date, weight: Number }],
+  foods: [{ type: mongoose.Schema.Types.ObjectId, ref: "Food" }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+});
---
+++ foods/foodModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  calories: {
+    type: Number,
+    required: true
+  },
+  proteins: {
+    type: Number,
+    required: true
+  },
+  fats: {
+    type: Number,
+    required: true
+  },
+  carbohydrates: {
+    type: Number,
+    required: true
+  },
+  vegan: Boolean,
+  vegetarian: Boolean
+});
+
+mongoose.Model("Food", foodSchema);
